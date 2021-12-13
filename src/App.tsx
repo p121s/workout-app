b97bdfb76dd30ff07edc428a0295d0d94fca22e8
p121s <p121s@mail.ru>
2021-12-13 15:05:50 +0300

    Fix code

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import { RootStateOrAny, useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { getDataFromAPI } from './redux/asyncActions';
-import './App.css';
 import ExercisesPage from './components/pages/ExercisesPage';
 import { Routes, Route } from 'react-router';
 import WorkoutPage from './components/pages/WorkoutPage';
+import './App.css';
 
 function App() {
   const dispatch = useDispatch();
-  const state = useSelector((state: RootStateOrAny) => state.data);
-  const [questions, setQuestions] = useState([]);
-
+  
   useEffect(() => {
     dispatch(getDataFromAPI());
   }, [dispatch]);
 
-  useEffect(() => {
-    setQuestions(state && state.data.questions);
-  }, [state]);
-
-  console.log(state);
-
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<ExercisesPage questions={questions} />} />
+        <Route path='/' element={<ExercisesPage />} />
         <Route path='/workout' element={<WorkoutPage />} />
       </Routes>
     </div>
