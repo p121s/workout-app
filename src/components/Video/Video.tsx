1158cb3e397c3a245d27542de19910deaee9cfae
Anton <p121s@mail.ru>
2022-01-03 19:13:32 +0300

    Fix code

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from "react";
 import { VideoProps } from "./Video.interfaces";
+import { VideoTag } from "./Video.styled";
 
 export default function Video({ exerciseVideo, isPause, isReady }: VideoProps): JSX.Element {
     const video = useRef<any>();
@@ -7,14 +8,14 @@ export default function Video({ exerciseVideo, isPause, isReady }: VideoProps): 
     useEffect(() => {
         if ((isPause && video.current) || isReady) {
             video.current.pause();
-        } else if ((!isPause && video.current) || !isReady) {
+        } else {
             video.current.play();
         }
     }, [isPause, isReady]);
 
     return (
-        <video ref={video} src={`${exerciseVideo}`} muted loop width="100%">
+        <VideoTag ref={video} src={`${exerciseVideo}`} muted loop>
             <source type="video/mp4" />
-        </video>
+        </VideoTag>
     );
 }
